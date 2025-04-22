a3adc255b156019211ea3e921c45207f7a650d17
rok <rexblanegamer@gmail.com>
2025-04-22 16:32:45 -0300

    feat: Now we have mixin!

---
+++ src/main/java/com/pedrorok/hypertube/mixin/PlayerModelMixin.java
@@ -0,0 +1,58 @@
+package com.pedrorok.hypertube.mixin;
+
+import com.pedrorok.hypertube.blocks.HypertubeBaseBlock;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * @author Rok, Pedro Lucas nmm. Created on 22/04/2025
+ * @project Create Hypertube
+ */
+@Mixin(PlayerModel.class)
+public abstract class PlayerModelMixin {
+    
+    @Inject(method = "setupAnim*", at = @At("RETURN"))
+    private void onSetupAnim(LivingEntity entity, float limbSwing, float limbSwingAmount,
+                             float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if (entity instanceof Player player
+            && player.getPersistentData().getBoolean(HypertubeBaseBlock.TRAVEL_TAG)) {
+            PlayerModel<?> model = (PlayerModel<?>) (Object) this;
+
+            model.rightArm.xRot = 0;
+            model.rightArm.yRot = 0;
+            model.rightArm.zRot = 0;
+
+            model.rightSleeve.xRot = 0;
+            model.rightSleeve.yRot = 0;
+            model.rightSleeve.zRot = 0;
+
+
+            model.leftArm.xRot = 0;
+            model.leftArm.yRot = 0;
+            model.leftArm.zRot = 0;
+
+            model.leftSleeve.xRot = 0;
+            model.leftSleeve.yRot = 0;
+            model.leftSleeve.zRot = 0;
+
+
+
+            model.rightLeg.xRot = 0;
+            model.rightLeg.yRot = 0;
+            model.rightLeg.zRot = 0;
+
+            model.leftLeg.xRot = 0;
+            model.leftLeg.yRot = 0;
+            model.leftLeg.zRot = 0;
+
+            model.body.xRot = 0;
+            model.body.yRot = 0;
+            model.body.zRot = 0;
+        }
+    }
+}
