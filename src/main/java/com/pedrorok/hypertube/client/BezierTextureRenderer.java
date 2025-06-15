662735553599006d6444e87c357130bc65a39bdc
rok <rexblanegamer@gmail.com>
2025-06-15 01:52:36 -0300

    fix: Fixing vertical zFight

--- src/main/java/com/pedrorok/hypertube/client/BezierTextureRenderer.java
+++ src/main/java/com/pedrorok/hypertube/client/BezierTextureRenderer.java
@@ -139,7 +139,7 @@ public class BezierTextureRenderer<T extends IBezierProvider> implements BlockEn
         float nz = (offset.z / radius) * normalMultiplier;
 
         if (zFightFix) {
-            pose.translate(0, 0.00001f, 0);
+            pose.translate(0.00001f, 0.00001f, 0.00001f);
         }
         builder.addVertex(pose, x, y, z)
                 .setColor(255, 255, 255, 255)
