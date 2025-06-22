afa342d171ada73b31112f190340fbefb07bbdcd
rok <rexblanegamer@gmail.com>
2025-06-22 13:52:44 -0300

    fix: Fixing player weird animation when sneaking inside the tube - Close #50

--- src/main/java/com/pedrorok/hypertube/mixin/core/LocalPlayerMixin.java
+++ src/main/java/com/pedrorok/hypertube/mixin/core/LocalPlayerMixin.java
@@ -1,7 +1,7 @@
 package com.pedrorok.hypertube.mixin.core;
 
 import com.pedrorok.hypertube.managers.travel.TravelConstants;
-import net.minecraft.world.entity.Pose;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,16 +9,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * @author Rok, Pedro Lucas nmm. Created on 22/04/2025
+ * @author Rok, Pedro Lucas nmm. Created on 21/06/2025
  * @project Create Hypertube
  */
-@Mixin(Player.class)
-public abstract class PlayerMovementMixin {
+@Mixin(LocalPlayer.class)
+public class LocalPlayerMixin {
 
-    @Inject(method = "canPlayerFitWithinBlocksAndEntitiesWhen", at = @At("HEAD"), cancellable = true)
-    private void onCanPlayerFitWithinBlocksAndEntitiesWhen(Pose p_294172_, CallbackInfoReturnable<Boolean> cir) {
-        Player player = (Player) (Object) this;
+    @Inject(method = "isShiftKeyDown", at = @At("HEAD"), cancellable = true)
+    private void cancelShiftKeyDown(CallbackInfoReturnable<Boolean> cir) {
+        LocalPlayer player = (LocalPlayer) (Object) this;
         if (!player.getPersistentData().getBoolean(TravelConstants.TRAVEL_TAG)) return;
-        cir.setReturnValue(true);
+        cir.setReturnValue(false);
     }
 }
