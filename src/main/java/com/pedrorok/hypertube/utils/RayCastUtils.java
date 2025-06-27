2052a93d61eeadc28a7b03414aab6807c6544abf
rok <rexblanegamer@gmail.com>
2025-06-27 12:56:15 -0300

    feat: Improving block placement (now you wont place blocks vertically when the path is blocked

--- src/main/java/com/pedrorok/hypertube/utils/RayCastUtils.java
+++ src/main/java/com/pedrorok/hypertube/utils/RayCastUtils.java
@@ -1,5 +1,6 @@
 package com.pedrorok.hypertube.utils;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.BlockHitResult;
@@ -21,21 +22,30 @@ public class RayCastUtils {
     public static Direction getDirectionFromHitResult(Player player, @Nullable Supplier<Boolean> filter, boolean ignoreFilter) {
         HitResult hitResult = player.pick(5, 0, false);
         if (hitResult.getType() != HitResult.Type.BLOCK) {
-            return getFromPlayer(player);
+            return getFromPlayer(player, null);
         }
         BlockHitResult blockHitResult = (BlockHitResult) hitResult;
         if (filter != null && (!filter.get() || ignoreFilter)) {
-            return getFromPlayer(player);
+            return getFromPlayer(player, blockHitResult.getBlockPos().relative(blockHitResult.getDirection()));
         }
         return blockHitResult.getDirection().getOpposite();
     }
 
-    private static Direction getFromPlayer(Player player) {
+    private static Direction getFromPlayer(Player player, BlockPos pos) {
         Direction direction = player.getDirection().getOpposite();
+        boolean usingUpOrDown = false;
         if (player.getXRot() < -45) {
             direction = Direction.DOWN;
+            usingUpOrDown = true;
         } else if (player.getXRot() > 45) {
             direction = Direction.UP;
+            usingUpOrDown = true;
+        }
+        if (pos != null && usingUpOrDown) {
+            BlockPos relative = pos.relative(direction.getOpposite());
+            if (player.level().getBlockState(relative).isSolid()) {
+                direction = player.getDirection().getOpposite();
+            }
         }
         return direction;
     }
