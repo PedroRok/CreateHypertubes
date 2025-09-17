4150525109bc9968059402bd68cdaea9590e9542
rok <rexblanegamer@gmail.com>
2025-09-17 17:17:50 -0300

    feat: prevent damage and invulnerability while traveling through hypertubes - close #92

--- src/main/java/com/pedrorok/hypertube/mixin/core/EntityTravelingMixin.java
+++ src/main/java/com/pedrorok/hypertube/mixin/core/EntityTravelingMixin.java
@@ -1,12 +1,14 @@
 package com.pedrorok.hypertube.mixin.core;
 
 import com.pedrorok.hypertube.core.travel.TravelConstants;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Pose;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
@@ -25,5 +27,19 @@ public class EntityTravelingMixin {
         if (!(((Entity) (Object) this) instanceof LivingEntity player)
             || !player.getPersistentData().getBoolean(TravelConstants.TRAVEL_TAG)) return;
         cir.setReturnValue(Pose.STANDING);
+    }
+
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    private void createHypertube$cancelHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (!(((Entity) (Object) this) instanceof LivingEntity entity)
+            || !entity.getPersistentData().getBoolean(TravelConstants.TRAVEL_TAG)) return;
+        cir.setReturnValue(false);
+    }
+
+    @Inject(method = "isInvulnerableTo", at = @At("HEAD"), cancellable = true)
+    private void createHypertube$cancelInvulnerableTo(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+        if (!(((Entity) (Object) this) instanceof LivingEntity entity)
+            || !entity.getPersistentData().getBoolean(TravelConstants.TRAVEL_TAG)) return;
+        cir.setReturnValue(true);
     }
 }
