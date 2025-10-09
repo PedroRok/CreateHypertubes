babc91063b1a3cf1e02e287b38c1ecd007b63968
rok <rexblanegamer@gmail.com>
2025-10-09 09:34:00 -0300

    fix: ensure ambient sound stops only on client side in HyperAcceleratorBlockEntity - Close #102

--- src/main/java/com/pedrorok/hypertube/blocks/blockentities/HyperAcceleratorBlockEntity.java
+++ src/main/java/com/pedrorok/hypertube/blocks/blockentities/HyperAcceleratorBlockEntity.java
@@ -193,8 +193,10 @@ public class HyperAcceleratorBlockEntity extends ActionTubeBlockEntity implement
     @Override
     public void remove() {
         super.remove();
-        TubeSoundManager.TubeAmbientSound sound = TubeSoundManager.getAmbientSound(tubeSoundId);
-        sound.stopSound();
+        if (level.isClientSide) {
+            TubeSoundManager.TubeAmbientSound sound = TubeSoundManager.getAmbientSound(tubeSoundId);
+            sound.stopSound();
+        }
     }
 
 
