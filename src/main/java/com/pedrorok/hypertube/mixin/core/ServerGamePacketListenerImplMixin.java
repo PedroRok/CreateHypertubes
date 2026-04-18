b3d9437610ed905caa922ad06bfb824b87774d2b
Ocelot <FinntheRaider@gmail.com>
2026-04-18 08:35:38 -0600

    Change redirect to wrap operation

--- src/main/java/com/pedrorok/hypertube/mixin/core/ServerGamePacketListenerImplMixin.java
+++ src/main/java/com/pedrorok/hypertube/mixin/core/ServerGamePacketListenerImplMixin.java
@@ -1,5 +1,7 @@
 package com.pedrorok.hypertube.mixin.core;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.pedrorok.hypertube.core.travel.TravelConstants;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
@@ -14,15 +16,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImplMixin {
 
-    @Redirect(
-            method = "handleMovePlayer",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/server/level/ServerPlayer;isChangingDimension()Z"
-            )
-    )
-    private boolean redirectIsChangingDimension(ServerPlayer player) {
-        if (player.getPersistentData().getBoolean(TravelConstants.TRAVEL_TAG)) return true;
-        return player.isChangingDimension();
+    /**
+     * FIXME: Don't just disable this check to handle sub-level freezing
+     */
+    @WrapOperation(method = "handleMovePlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;isChangingDimension()Z"))
+    private boolean redirectIsChangingDimension(ServerPlayer instance, Operation<Boolean> original) {
+        if (instance.getPersistentData().getBoolean(TravelConstants.TRAVEL_TAG)) return true;
+        return original.call(instance);
     }
 }
